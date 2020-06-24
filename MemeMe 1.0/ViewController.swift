801f9f1c2275a0597f81cd1e7748e81c52872119
Nitin Anand <ni3@nitins-mac-mini.local>
2020-06-25 02:38:23 +0530

    Initial Commit

---
+++ MemeMe 1.0/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MemeMe 1.0
+//
+//  Created by Nitin Anand on 25/06/20.
+//  Copyright © 2020 Ni3X. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
